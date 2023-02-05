12cccf74b5b4ef27383c7707e357f6bda54ea97b
roman <romanvafin92@gmail.com>
2023-02-05 22:49:07 +0300

    ДЗ на проверку

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,27 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
-        ... // Пишем код для задачи 1
+        String firstName = "Иван";
+        String middleName = "Иванович";
+        String lastName = "Иванов";
+        String fullName = lastName + " "
+                + firstName + " "
+                + middleName + " ";
+
+        System.out.println("ФИО сотрудника — " + fullName);
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
-        ... // Пишем код для задачи 2
+        String fullName = "Ivanov Ivan Ivanovich";
+        String fullNameUpper = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUpper);
     }
 
     public static void task3 () {
         System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 }
